195704d0258dda31c072c05d8de984d6dd0bcd88
Shubham Yadav <84612420+shubhamkagithub@users.noreply.github.com>
2025-03-03 12:29:36 +0530

    fixing port_security (check box) bug

--- openstack_dashboard/api/neutron.py
+++ openstack_dashboard/api/neutron.py
@@ -72,6 +72,10 @@ class NeutronAPIDictWrapper(base.APIDictWrapper):
                 apidict['admin_state'] = 'UP'
             else:
                 apidict['admin_state'] = 'DOWN'
+        # https://bugs.launchpad.net/horizon/+bug/2093367
+        if 'is_port_security_enabled' in apidict:
+            if apidict['is_port_security_enabled']:
+                apidict['port_security_enabled'] = 'UP'
 
         # Django cannot handle a key name with ':', so use '__'.
         apidict.update({
